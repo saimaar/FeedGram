3f667f1e7970bf46b4686ce16eb8f7a47867894d
saimaar <rahman.saima92@gmail.com>
2021-02-26 00:25:28 -0500

    removed require dot env since react does not meed it

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,13 +1,13 @@
-require('dotenv').config()
+
 
 var firebaseConfig = {
-    apiKey: ENV["API_KEY"],
+    apiKey: process.env.API_KEY,
     authDomain: "feedgram-co.firebaseapp.com",
-    databaseURL: ENV["DATABASE_URL"],
+    databaseURL: process.env.DATABASE_URL,
     projectId: "feedgram-co",
     storageBucket: "feedgram-co.appspot.com",
-    messagingSenderId: ENV["SENDER_ID"],
-    appId: ENV["APP_ID"]
+    messagingSenderId: process.env.SENDER_ID,
+    appId: process.env.APP_ID
   };
 
   const firebase = window.firebase.initializeApp(firebaseConfig);
