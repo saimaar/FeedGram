e01cc9d9fedb9786e350eaf1e4ba447dc5661513
saimaar <rahman.saima92@gmail.com>
2021-02-26 00:02:57 -0500

    firebase setup complete

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,1 +1,14 @@
 require('dotenv').config()
+
+var firebaseConfig = {
+    apiKey: ENV["API_KEY"],
+    authDomain: "feedgram-co.firebaseapp.com",
+    databaseURL: ENV["DATABASE_URL"],
+    projectId: "feedgram-co",
+    storageBucket: "feedgram-co.appspot.com",
+    messagingSenderId: ENV["SENDER_ID"],
+    appId: ENV["APP_ID"]
+  };
+
+  const firebase = window.firebase.initializeApp(firebaseConfig);
+  export { firebase };
